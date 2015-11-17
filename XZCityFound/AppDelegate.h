6a66bcebf7d110d1d5f1a3d273ed0a82a21822af
zhenzhen <zhenzhen@185.com>
2015-11-17 22:03:42 +0800

    Initial Commit

---
+++ XZCityFound/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZCityFound
+//
+//  Created by 珍 on 15/11/17.
+//  Copyright © 2015年 UME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZCityFound/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZCityFound
+//
+//  Created by 珍 on 15/11/17.
+//  Copyright © 2015年 UME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
